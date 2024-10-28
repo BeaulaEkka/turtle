7dd350855d7a1242cee1476c45f152ac3fbbe9c3
Your Name <ekka.beaula@gmail.com>
2024-10-28 11:41:27 +0100

    singly linked lists

--- car.py
+++ car.py
@@ -1,13 +1,25 @@
+from typing import Self
+
+
 class Car:
-    def __init__(self, color: str, horse_power: int) -> None:
-        self.color = color
+    def __init__(self, brand: str, horse_power: int) -> None:
+        self.brand = brand
         self.horse_power = horse_power
 
+    def drive(self) -> None:
+        print(f'{self.brand} is driving!')
 
-volvo: Car = Car('red', 200)
-print(volvo.color)
-print(volvo.horse_power)
+    def get_info(self, var: int) -> None:
+        print(var)
+        print(f'{self.brand} with {self.horse_power} horsepower')
 
-mercedes:Car=Car('green',300)
-print(mercedes.color)
-print(mercedes.horse_power)
+    def __str__(self) -> str:
+        return f'{self.brand} with {self.horse_power} horsepower: string method'
+
+    def __add__(self, other: Self) -> str:
+        return f'{self.brand} + {other.brand} = {self.brand} & {other.brand}'
+
+
+volvo: Car = Car('Volvo', 200)
+bmw:Car=Car('bmw',300)
+print(volvo + bmw)
