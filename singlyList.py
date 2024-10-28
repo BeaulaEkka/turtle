619901a30f5d5aafa42ae862fab95cf47c5e97f8
Your Name <ekka.beaula@gmail.com>
2024-10-29 00:00:48 +0100

    string

--- singlyList.py
+++ singlyList.py
@@ -39,4 +39,25 @@ secondInList = linkedList.insert(second_node)
 third_node = Node('Shally')
 thirdInList = linkedList.insert(third_node)
 
+fourth_node = Node('Dan')
+thirdInList = linkedList.insert(fourth_node)
+
 print(linkedList)
+
+# search for names
+def search(head, searchParameter):
+    current_node = head
+    while current_node:
+        if current_node.data == searchParameter:
+            return True
+        current_node = current_node.next_node
+
+    return False
+
+
+ben_is_found = search(linkedList.head, 'Ben')
+Danny_is_found = search(linkedList.head, 'Danny')
+Randy_is_found = search(linkedList.head, 'Randy')
+print(f"Is 'Ben' found? {ben_is_found}")
+print(f"Is 'Danny' found? {Danny_is_found}")
+print(f'is Randy found? {Randy_is_found}')
