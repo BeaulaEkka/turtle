b9635649454fd7bf72bd9e006eda79bc79c7d310
Your Name <ekka.beaula@gmail.com>
2024-10-30 22:32:07 +0100

    doubly insert at tail

--- doublyLinkedLists.py
+++ doublyLinkedLists.py
@@ -1,38 +1,53 @@
 class DoublyNode:
-    def __init__(self, val, next=None, prev=None):
+    def __init__(self, val, next_node=None, prev_node=None):
         self.val = val
-        self.next = next
-        self.prev = prev
+        self.next_node = next_node
+        self.prev_node = prev_node
 
     def __str__(self):
         return str(self.val)
 
 
 def display(head):
-    current = head
+    current_node = head
     elements = []
-    while current:
-        elements.append(str(current.val))
-        current = current.next
+    while current_node:
+        elements.append(str(current_node.val))
+        current_node = current_node.next_node
     print(' <--> '.join(elements))
 
 
 # Insert at the beginning
 def insert_at_beginning(head, tail, val):
-    new_node = DoublyNode(val, next=head)
+    new_node = DoublyNode(val, next_node=head)
     if head:
-        head.prev = new_node
+        head.prev_node = new_node
     else:
         tail = new_node  # If the list is empty, the new node is also the tail
     return new_node, tail
 
 
+# Insert at the tail
+def insert_at_tail(head, tail, val):
+    new_node = DoublyNode(val, prev_node=tail)
+    if tail:
+        tail.next_node = new_node
+    else:
+        head = new_node  # If the list is empty, the new node is also the head
+    return head, new_node  # Always return the head and new tail
+
+
 # Creating a doubly linked list with one node
 head = tail = DoublyNode(1)
 
-# Insert a new node at the beginning
+# Insert new nodes at the beginning
 head, tail = insert_at_beginning(head, tail, 3)
 head, tail = insert_at_beginning(head, tail, 5)
+head, tail = insert_at_beginning(head, tail, 10)
+head, tail = insert_at_beginning(head, tail, 7)
+
+# Insert a node at the tail
+head, tail = insert_at_tail(head, tail, 40)
 
 # Display the list
 display(head)
