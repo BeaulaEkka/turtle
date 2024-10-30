396abf94b333239267dce99889b340e127c1cbaf
Your Name <ekka.beaula@gmail.com>
2024-10-30 09:04:42 +0100

    insert_at_begining

--- doublyLinkedLists.py
+++ doublyLinkedLists.py
@@ -6,7 +6,33 @@ class DoublyNode:
 
     def __str__(self):
         return str(self.val)
-    
 
+
+def display(head):
+    current = head
+    elements = []
+    while current:
+        elements.append(str(current.val))
+        current = current.next
+    print(' <--> '.join(elements))
+
+
+# Insert at the beginning
+def insert_at_beginning(head, tail, val):
+    new_node = DoublyNode(val, next=head)
+    if head:
+        head.prev = new_node
+    else:
+        tail = new_node  # If the list is empty, the new node is also the tail
+    return new_node, tail
+
+
+# Creating a doubly linked list with one node
 head = tail = DoublyNode(1)
-print(head)
+
+# Insert a new node at the beginning
+head, tail = insert_at_beginning(head, tail, 3)
+head, tail = insert_at_beginning(head, tail, 5)
+
+# Display the list
+display(head)
