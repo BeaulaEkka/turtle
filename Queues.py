b966cd1b2af766b11f4831f9735ee2f077ba57c3
Your Name <ekka.beaula@gmail.com>
2024-11-01 18:10:34 +0100

    queue- end

--- Queues.py
+++ Queues.py
@@ -1,5 +1,22 @@
-#Queues-First in first out(FIFO)
+# Queues-First in first out(FIFO)-O(1)
 from collections import deque
 
-q=deque()
-print( q)
+q = deque()
+
+
+# Enqueue-Add emements to the right -o(1)
+q.append(1)
+q.append(2)
+q.append(3)
+q.append(4)
+
+# Dequeue-Remove an element from the left-O(1)
+q.popleft()
+
+
+# Peek from left side-O(1)
+print(q[0])
+
+# peek from the right side-O(1)
+print(q[-1])
+print(q)
