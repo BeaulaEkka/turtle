fb04c85feb937dc0385899a6a07c50482232da62
Your Name <ekka.beaula@gmail.com>
2024-11-05 00:31:32 +0100

    sum_digits_recursive-end

--- sum_digits.py
+++ sum_digits.py
@@ -1,11 +1,11 @@
 def sum_digits(n):
-  if n < 0:
-    print('Inputs must be greater than 0')
-  result = 0
-  while n != 0:
-    result += n % 10
-    n = n//10
-    return result+n
+    if n < 0:
+        print('Inputs must be greater than 0')
+    result = 0
+    while n != 0:
+        result += n % 10
+        n = n//10
+        return result+n
 
 
 # test cases
@@ -15,4 +15,13 @@ print(sum_digits(123456789) == 45)
 print(sum_digits(32))
 print(sum_digits(-1))
 
-#recrusive
+# recrusive
+
+
+def sum_digits_recursive(n):
+    if n == 0:
+        return 0
+    return n % 10+sum_digits_recursive(n//10)
+
+
+print(f'sum_digits_recursive:{sum_digits_recursive(52)}')
