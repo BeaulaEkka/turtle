c4eb412d7e49ea17fd8fe056d4ce2bc422ce9fa8
Your Name <ekka.beaula@gmail.com>
2024-11-05 12:35:24 +0100

    bruteForce-end

--- bruteForce.py
+++ bruteForce.py
@@ -1,1 +1,4 @@
 # Brute Force Algorithms
+'''We have all heard of the saying, “Work smarter, not harder”. This phrase is not only sound advice for everyday life but also for programming. After all, the reason why we study algorithms is to learn smarter ways to solve problems. A “smart” algorithm is fast and often complex; on the other hand, a “brute force“ algorithm is slow but much easier to implement.
+
+As its name implies, a brute force algorithm is a straightforward method that solves a problem by going through every possible choice one by one until a solution is found. Instead of utilizing clever techniques, brute force algorithms rely on sheer computing power to solve problems. In this article, we will learn about brute force algorithms and analyze their pros and cons.'''
