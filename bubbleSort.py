348db7b56611195f4a92be1a5fd84b487d11a181
Your Name <ekka.beaula@gmail.com>
2024-11-06 20:36:06 +0100

    selectionSort-start

--- bubbleSort.py
+++ bubbleSort.py
@@ -8,18 +8,18 @@ def bubble_sort(arr):
     while flag:  # Loop until no swaps occur
         flag = False
         for i in range(1, n):
-            if arr[i-1] > arr[i]:  
-                flag = True  
-                arr[i-1], arr[i] = arr[i], arr[i-1]   
-    return arr  
+            if arr[i-1] > arr[i]:
+                flag = True
+                arr[i-1], arr[i] = arr[i], arr[i-1]
+    return arr
 
 
 A = [-5, 3, 2, 1, -3, -3, 7, 2, 2]
 sorted_array = bubble_sort(A)  # Sort the array
 print("Sorted Array:", sorted_array)  # Print the sorted result
-## Optimized 
-#The optimized version of bubble sort will have a more efficient big O runtime than the unoptimized version of bubble sort.
-#Code
+# Optimized
+# The optimized version of bubble sort will have a more efficient big O runtime than the unoptimized version of bubble sort.
+# Code
 
 
 def swap(arr, index_1, index_2):
@@ -42,16 +42,28 @@ def bubble_sort_optimized(arr):
                 arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
 
 
-#insertion Sort-O(n^2)
-#space-O(1)
+# insertion Sort-O(n^2)
+# space-O(1)
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if arr[j-1] > arr[j]:
+                arr[j-1], arr[j] = arr[j], arr[j-1]
+            else:
+                break
 
 
-# Example array
+#selection Sort
+
+
+            # Example array
 A = [-5, 3, 2, 1, -3, -3, 7, 2, 2]
 
 # Copy the array for both sorting methods
 A_unoptimized = A.copy()
 A_optimized = A.copy()
+A_insertion = A.copy()
 
 # Sort using the unoptimized bubble sort
 bubble_sort_unoptimized(A_unoptimized)
@@ -60,3 +72,6 @@ print("Unoptimized Sorted Array:", A_unoptimized)
 # Sort using the optimized bubble sort
 bubble_sort_optimized(A_optimized)
 print("Optimized Sorted Array:", A_optimized)
+
+insertion_sort(A_insertion)
+print('A_insertion', A_insertion)
