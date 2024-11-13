4d5a3f99f6d8ca5b35a3a230e3106c5ae5422a8e
Your Name <ekka.beaula@gmail.com>
2024-11-13 23:41:17 +0100

    heappush

--- depthFirstSearch.py
+++ depthFirstSearch.py
@@ -37,3 +37,8 @@ A = [-4, 3, 1, 0, 2, 5, 10, 8, 12, 9]
 heapq.heapify(A)
 print(A)
 # [-4, 0, 1, 3, 2, 5, 10, 8, 12, 9] not completely in ascending order
+
+#Heap Push(insert an element)
+# Time:O(logn),space:O(1)
+heapq.heappush(A,4)
+print(f'heappush A:{A}')
