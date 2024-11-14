3033fafaf404d47eef5c7588fb72d7b38837fab0
Your Name <ekka.beaula@gmail.com>
2024-11-14 14:12:59 +0100

    pseudocode

---
+++ heapSort.py
@@ -0,0 +1,22 @@
+'''
+pseudocode:
+siftdown(list,parent,idx,upper):
+repeat:
+    if parent has two children:
+        if parent > both children:
+            end subroutine
+    else:
+        swap parent eith greater child
+        set parent idx to child idx
+    else if parent has one child:
+        if parent>child:
+            end subroutine
+    else:
+        swap parent with greater child
+        set parent idx to child idx
+    else:
+        parent has no child, end subroutine
+
+
+
+'''
--- maxHeap.py
+++ maxHeap.py
@@ -1,36 +1,47 @@
 class MaxHeap:
-  def __init__(self):
-    self.heap_list = [None]
-    self.count = 0
+    def __init__(self):
+        self.heap_list = [None]
+        self.count = 0
 
-  # HEAP HELPER METHODS
-  # DO NOT CHANGE!
-  def parent_idx(self, idx):
-    return idx // 2
+    # HEAP HELPER METHODS
+    # DO NOT CHANGE!
+    def parent_idx(self, idx):
+        return idx // 2
 
-  def left_child_idx(self, idx):
-    return idx * 2
+    def left_child_idx(self, idx):
+        return idx * 2
 
-  def right_child_idx(self, idx):
-    return idx * 2 + 1
+    def right_child_idx(self, idx):
+        return idx * 2 + 1
 
-  # END OF HEAP HELPER METHODS
+    # END OF HEAP HELPER METHODS
 
-  def add(self, element):
-    self.count += 1
-    print("Adding: {0} to {1}".format(element, self.heap_list))
-    self.heap_list.append(element)
-    self.heapify_up()
+    def add(self, element):
+        self.count += 1
+        print(f'Adding: {element} to {self.heap_list}')
+        self.heap_list.append(element)
+        self.heapify_up()
 
-  def heapify_up(self):
-    print("Heapifying up")
-    idx = self.count
-    while self.parent_idx(idx) > 0:
-      child = self.heap_list[idx]
-      parent = self.heap_list[self.parent_idx(idx)]
-      if parent < child:
-        print("swapping {0} with {1}".format(parent, child))
-        self.heap_list[idx] = parent
-        self.heap_list[self.parent_idx(idx)] = child
-      idx = self.parent_idx(idx)
-    print("Heap Restored {0}".format(self.heap_list))
+    def heapify_up(self):
+        print("Heapifying up")
+        idx = self.count
+        print(f'heapify idx=self.count:{idx}')
+        while self.parent_idx(idx) > 0:
+            child = self.heap_list[idx]
+            print(f'child:{child}')
+            parent = self.heap_list[self.parent_idx(idx)]
+            print(f'parent:{parent}')
+            if parent < child:
+                print("swapping {0} with {1}".format(parent, child))
+                self.heap_list[idx] = parent
+                self.heap_list[self.parent_idx(idx)] = child
+            idx = self.parent_idx(idx)
+        print("Heap Restored {0}".format(self.heap_list))
+
+
+heap = MaxHeap()
+heap.add(5)
+heap.add(10)
+
+
+print(f'heap_list:{heap.heap_list}')
