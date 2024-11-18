f0421fe6efa82ba66443b247d0f90ab07f54bd90
Your Name <ekka.beaula@gmail.com>
2024-11-18 21:27:18 +0100

    travelling salesperson-start

--- greedy.py
+++ greedy.py
@@ -1,2 +1,17 @@
 # greedy Algorithms Tutorial
-#Bulbs
+# Bulbs
+'''given N bulbs, itther on (1) or off(0).
+Turning on ith bulb causes all remaining bulbs on the right to flip.
+
+Find the min number of switches to turla all the bulbs on.
+
+constraints:
+1>N<=1e5 # n of bulbs are between 1 and 10to the power 5
+A[i]={0,1} # the bulb or the bit is 0 or 1
+
+'''
+# Traveling Salesperson
+from sys import maxsize
+from itertools import permutations
+from math import factorial
+V = 4
